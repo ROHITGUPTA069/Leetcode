8e22d7c9542a7e86d4e018b22e7996129cf4a7c9
Rohit Gupta <rohitgupta5tbaksa@gmail.com>
2025-07-17 19:58:21 +0530

    solved some more questions

--- src/LinkedList/questions.java
+++ src/LinkedList/questions.java
@@ -3,21 +3,6 @@ package LinkedList;
 //leetcode questions
 
 public class removeDublicate {
-    public static void main(String[] args) {
-        ll list1 = new ll();
-        ll list2 = new ll();
-        list1.insertfirst(1);
-        list1.insertLast(2);
-        list1.insertLast(2);
-        list2.insertLast(3);
-        list2.insertLast(4);
-        list2.insertLast(4);
-        ll list3 = ll.combinelist(list1, list2);
-        list3.display();
-//        list.display();
-//        ll.dublicate();
-//        list.display();
-    }
 
     public static class ll {
 
@@ -39,6 +24,9 @@ public class removeDublicate {
             Node(int value, Node next){
                 this.value = value;
                 this.next = next;
+            }
+            Node(){
+
             }
         }
 
@@ -115,7 +103,7 @@ public class removeDublicate {
         }
 
 
-
+        ////returns the node of the cycle starts
         public static Node detectCycle(Node head) {
             Node slow = head;
             Node fast = head.next;
@@ -141,6 +129,8 @@ public class removeDublicate {
             return s;
         }
 
+
+        /// Returns the length of the cycle
         public static int lengthCycle(Node head){
             Node node = head;
             if(node.next == null){
@@ -153,6 +143,81 @@ public class removeDublicate {
                 length++;
             }
             return length;
+        }
+
+        /// detects the ll has a  cycle or not
+        public static boolean cycleFound(Node head){
+            Node node = head;
+            if(node == null || node.next == null){
+                return false;
+            }
+            Node slow = node;
+            Node fast = node.next;
+            while(slow != fast){
+                if(fast.next == null || fast.next.next == null) return false;
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return true;
+        }
+
+        /// detect happy number
+        public boolean isHappy(int n) {
+            while(n != 1){
+                int q = n / 10;
+                int r = n % 10;
+                n = q + r;
+                if(n == 1){
+                    break;
+                }
+                Node node = new Node(n);
+                insertLast(node.value);
+                if(cycleFound(node)){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// mergetwolist using node
+        public Node mergeTwoLists(Node list1, Node list2) {
+            Node ans = new Node();
+            //since the node will keep on updating so the current will be the tail
+            Node tail = ans;
+            while(list1 != null && list2 != null){
+                if(list1.value < list2.value){
+                    tail.next = list1;
+                    list1 = list1.next;
+                    tail = tail.next;
+                }
+                else{
+                    tail.next = list2;
+                    list2 = list2.next;
+                    tail = tail.next;
+                }
+            }
+            while(list1 != null){
+                tail.next = list1;
+                tail = tail.next;
+                list1 = list1.next;
+            }
+            while(list2 != null){
+                tail.next = list2;
+                tail = tail.next;
+                list2 = list2.next;
+            }
+            return ans.next;
+        }
+
+        /// find the middle node
+        public Node middleNode(Node head) {
+            Node slow = head;
+            Node fast = head;
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
         }
     }
 }
